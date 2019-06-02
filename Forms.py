24568bf0af8ec5ea13fab8874e5fee814b6a2076
kuzdman <kuzdman17@yandex.ru>
2019-06-02 22:28:05 +0500

    barcode generator module

--- Forms.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired
-
-
-class SingInForm(FlaskForm):
-    username = StringField(validators=[DataRequired()])
-    password = PasswordField(validators=[DataRequired()])
-    submit = SubmitField('Войти')
